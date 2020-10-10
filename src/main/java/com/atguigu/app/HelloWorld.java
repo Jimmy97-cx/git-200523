6eea953d95d60456a6b423c60f35894c6c6ad690
jimmy <jimmy@atguigu.com>
2020-10-10 09:16:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/app/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.atguigu.app;
+
+/**
+ * @author Jimmy
+ * @create 2020-10-10 9:09
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
